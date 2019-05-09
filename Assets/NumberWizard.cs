bbf589662739e52e1ec25a8af3e117d862e2bcf4
SpencerFaust <faus0059@umn.edu>
2019-05-09 13:09:52 -0500

    First variable added.

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,9 +7,10 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
         Debug.Log("Welcome to Spencer's Number Wizard!");
         Debug.Log("Please pick a number; don't tell me what you've picked!");
-        Debug.Log("The highest number you can pick is 1,000.");
+        Debug.Log("The highest number you can pick is: " + max);
         Debug.Log("The lowest number you can pisk is 0.");
     }
 
